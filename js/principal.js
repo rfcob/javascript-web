f1976da2b718631f51e0f99d2f324cbe613e8b04
Rafael Cobo <107076651+rfcob@users.noreply.github.com>
2022-10-25 20:46:07 -0300

    Add files via upload

--- js/principal.js
+++ js/principal.js
@@ -1,1 +1,35 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista"
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+
+var tdAltura= paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 200){
+	//console.log("Peso Inválido")
+	pesoEhValido = false;
+	tdImc.textContent = "Peso Inválido";
+}
+
+if (altura <= 0 || altura >= 3.00 ){
+	//console.log("Altura Inválido")
+	alturaEhValida = false;
+	tdImc.textContent = "Altura Inválida";
+}
+
+
+if(alturaEhValida && pesoEhValido){
+	var imc = peso / (altura * altura);
+	tdImc.textContent = imc;
+}
 
